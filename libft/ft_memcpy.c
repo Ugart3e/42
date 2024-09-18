dfbb9366f9957ff94e3295c43aae5ccbeacd98ca
Ugart3e <101062817+Ugart3e@users.noreply.github.com>
2024-09-18 11:38:34 +0200

    memcpy

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -3,10 +3,11 @@ void    *ft_memcpy(void *destination, const void *source, int num)
     unsigned char   *dest;
     unsigned char   *src;
 
+    if (!dest || !src)
+        return (0);
     dest = (unsigned char *)destination;
     src = (unsigned char *)source;
-    while (num--)
-    {
-        *dest = *src;
-    }
+    while (0 < num--)
+        *(dest++) = *(src++);
+    return ((void *)destination);
 }
