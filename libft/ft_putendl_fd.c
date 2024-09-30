2775879316745207f29539113b3bd77f64c291c6
Jose Luis Ugarte Galvez <jougarte@c2r4s6.42malaga.com>
2024-09-30 19:29:37 +0200

    holatodas

---
+++ libft/ft_putendl_fd.c
@@ -0,0 +1,12 @@
+void    ft_putendl_fd(char *str, int fd)
+{
+    int    i;
+    
+    i = 0;
+    while (str[i])
+    {
+        write(fd, &str[i], 1);
+        i++;
+    }
+    write(fd, "\n", 1);    
+}
---
+++ libft/ft_putstr_fd.c
@@ -0,0 +1,11 @@
+void    ft_putstr_fd(char *str, int fd)
+{
+    int i;
+    
+    i = 0;
+    while (str[i])
+    {
+        write(fd, &str[i], 1);
+        i++;
+    }
+}
