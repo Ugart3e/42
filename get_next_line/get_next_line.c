deff561607c1b5e0b98a067a4dae599de0a005a8
Ugart3e <101062817+Ugart3e@users.noreply.github.com>
2024-11-05 13:07:50 +0100

    gnl

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jougarte <jougarte@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 01:15:16 by jougarte          #+#    #+#             */
-/*   Updated: 2024/10/28 01:15:17 by jougarte         ###   ########.fr       */
+/*   Updated: 2024/11/02 12:56:25 by jougarte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,9 @@ char	*ft_get_next_line(int fd)
 	pos = 0;
 	if (fd == -1)
         return (NULL);
-	while ((read(fd, BUFF_SIZE, sizeof(char))) != '\n')
+	while ((read(fd, BUFFER_SIZE, sizeof(char))) != '\n')
 	{
+
   	}
 }
 
