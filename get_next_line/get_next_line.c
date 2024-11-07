0508911a276853d5ab4197598f75009c14ecbe15
jougarte <jougarte@student.42malaga.com>
2024-11-07 18:23:32 +0100

    gnll

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,30 +6,35 @@
 /*   By: jougarte <jougarte@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 01:15:16 by jougarte          #+#    #+#             */
-/*   Updated: 2024/11/02 12:56:25 by jougarte         ###   ########.fr       */
+/*   Updated: 2024/11/07 16:46:06 by jougarte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //read malloc free
 //return linea leida o null
+#include <stdio.h>
+#include "get_next_line.h"
 
-#include <unistd.h>
 char	*ft_get_next_line(int fd)
 {
-	static int	pos;
-	char		*line;
-
-	pos = 0;
-	if (fd == -1)
-        return (NULL);
-	while ((read(fd, BUFFER_SIZE, sizeof(char))) != '\n')
-	{
-
-  	}
+	static char	*line;
+	
+	line = malloc (BUFFER_SIZE + 1);
+	read(fd, line, BUFFER_SIZE);
+	return line;
+	
 }
 
 int main ()
 {
-	char r = ft_get_next_line(open("txt.txt", 1));
-	return (0);
+	int fd = open("txt.txt", O_RDONLY);
+	//r = linea 
+	for (size_t i = 0; i < 10; i++)
+	{
+		char *r = ft_get_next_line(fd);
+		printf("%s", r);
+
+	}
+			return (0);
+
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,13 +6,15 @@
 /*   By: jougarte <jougarte@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 00:33:21 by jougarte          #+#    #+#             */
-/*   Updated: 2024/10/28 00:33:23 by jougarte         ###   ########.fr       */
+/*   Updated: 2024/11/07 12:35:39 by jougarte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 5
+# endif
 # include<unistd.h>
 # include<stdio.h>
 # include<stdlib.h>
@@ -25,6 +27,5 @@ char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strdup(char *s1);
 char	*ft_strchr(char *s, int c);
 char	*ft_substr(char *s, unsigned int start, size_t len);
-char	*get_next_line(int fd);
-char	*ft_free(char **str);
+char	*ft_get_next_line(int fd);
 #endif
