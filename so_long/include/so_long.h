43a17f3890468d74b297d52a029edd850856ce94
Ugart3e <101062817+Ugart3e@users.noreply.github.com>
2025-05-20 14:35:59 +0200

    solong.h

--- so_long/include/so_long.h
+++ so_long/include/so_long.h
@@ -1,5 +1,53 @@
+#ifndef SO_LONG_H
+# define SO_LONG_H
+# include "../libs/libft/src/libft.h"
+# include "../libs/MLX42/include/MLX42/MLX42.h"
 # include <unistd.h>
-# include <stdio.h>
 # include <stdlib.h>
-# include <fcntl.h>
-# include <limits.h>
+# include <stdio.h>
+# include <string.h>
+
+# define PXL 50
+
+typedef struct s_p
+{
+	int	y;
+	int	x;
+}	t_p;
+
+typedef struct s_img
+{
+	mlx_texture_t	*coll;
+	mlx_texture_t	*wall;
+	mlx_texture_t	*exit_c;
+	mlx_texture_t	*exit_o;
+	mlx_texture_t	*p;
+	mlx_texture_t	*tile;
+	mlx_image_t		*collect_i;
+	mlx_image_t		*wall_i;
+	mlx_image_t		*exit_c_i;
+	mlx_image_t		*exit_o_i;
+	mlx_image_t		*p_i;
+	mlx_image_t		*ti_i;
+}	t_img;
+
+typedef struct s_map
+{
+	char		**map;
+	char		**map_save;
+	int			moves;
+	t_img		img;
+	t_p			p;
+	int			exit;
+	int			exity;
+	int			exitx;
+	int			exit_c;
+	int			coin;
+	int			coin_c;
+	int			p_num;
+	int			p_c;
+	int			height;
+	int			width;
+	int			null_check;
+	mlx_t		*wind;
+}	t_map;
