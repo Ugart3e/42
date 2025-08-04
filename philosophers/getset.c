297baf34907c51f6a3fd9663636793a0c79d3aa9
jougarte <jougarte@student.42malaga.com>
2025-08-04 16:50:14 +0200

    norminette getset

--- philosophers/getset.c
+++ philosophers/getset.c
@@ -1,38 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   getset.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jougarte <jougarte@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/04 16:47:37 by jougarte          #+#    #+#             */
+/*   Updated: 2025/08/04 16:50:01 by jougarte         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
 
-void set_bool(t_mtx *mutex, bool *dest, bool value)
+void	set_bool(t_mtx *mutex, bool *dest, bool value)
 {
-    safe_mutex_handle(mutex, LOCK);
-    *dest = value;
-    safe_mutex_handle(mutex, UNLOCK);
+	safe_mutex_handle(mutex, LOCK);
+	*dest = value;
+	safe_mutex_handle(mutex, UNLOCK);
 }
 
-bool get_bool(t_mtx *mutex, bool *value)
+bool	get_bool(t_mtx *mutex, bool *value)
 {
-    bool ret;
-    safe_mutex_handle(mutex, LOCK);
-    ret = *value;
-    safe_mutex_handle(mutex, UNLOCK);
-    return (ret);
+	bool	ret;
+
+	safe_mutex_handle(mutex, LOCK);
+	ret = *value;
+	safe_mutex_handle(mutex, UNLOCK);
+	return (ret);
 }
 
-void set_long(t_mtx *mutex, long *dest, long value)
+void	set_long(t_mtx *mutex, long *dest, long value)
 {
-    safe_mutex_handle(mutex, LOCK);
-    *dest = value;
-    safe_mutex_handle(mutex, UNLOCK);
+	safe_mutex_handle(mutex, LOCK);
+	*dest = value;
+	safe_mutex_handle(mutex, UNLOCK);
 }
 
-long get_long(t_mtx *mutex, long *value)
+long	get_long(t_mtx *mutex, long *value)
 {
-    long ret;
-    safe_mutex_handle(mutex, LOCK);
-    ret = *value;
-    safe_mutex_handle(mutex, UNLOCK);
-    return (ret);
+	long	ret;
+
+	safe_mutex_handle(mutex, LOCK);
+	ret = *value;
+	safe_mutex_handle(mutex, UNLOCK);
+	return (ret);
 }
 
-bool simulation_finished(t_table *table)
+bool	simulation_finished(t_table *table)
 {
-    return(get_bool(&table->table_mutex, &table->end_simulation));
+	return (get_bool(&table->table_mutex, &table->end_simulation));
 }
