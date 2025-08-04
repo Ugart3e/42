9a4cc4307bfd9e137f2c09575789c000e88049bf
jougarte <jougarte@student.42malaga.com>
2025-08-04 17:53:49 +0200

    norminette final

--- philosophers/init.c
+++ philosophers/init.c
@@ -6,7 +6,7 @@
 /*   By: jougarte <jougarte@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 16:50:36 by jougarte          #+#    #+#             */
-/*   Updated: 2025/08/04 17:27:55 by jougarte         ###   ########.fr       */
+/*   Updated: 2025/08/04 17:53:30 by jougarte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,11 +69,11 @@ void	data_init(t_table *table)
 
 void	lonely_init(t_table *table)
 {
-		table->start_simulation_time = gettime(MILISECOND);
-		set_bool(&table->table_mutex, &table->threads_ready, true);
-		safe_thread_handle(&table->philos[0].thread_id,
-			lonely, &table->philos[0], CREATE);
-		safe_thread_handle(&table->monitor, monitor_dinner, table, CREATE);
-		safe_thread_handle(&table->philos[0].thread_id, NULL, NULL, JOIN);
-		safe_thread_handle(&table->monitor, NULL, NULL, JOIN);
+	table->start_simulation_time = gettime(MILISECOND);
+	set_bool(&table->table_mutex, &table->threads_ready, true);
+	safe_thread_handle(&table->philos[0].thread_id,
+		lonely, &table->philos[0], CREATE);
+	safe_thread_handle(&table->monitor, monitor_dinner, table, CREATE);
+	safe_thread_handle(&table->philos[0].thread_id, NULL, NULL, JOIN);
+	safe_thread_handle(&table->monitor, NULL, NULL, JOIN);
 }
--- philosophers/write.c
+++ philosophers/write.c
@@ -1,49 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   write.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jougarte <jougarte@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/04 17:47:52 by jougarte          #+#    #+#             */
+/*   Updated: 2025/08/04 17:53:37 by jougarte         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
 
-static void debug_status(t_philo_state status, t_philo *philo, long elapsed)
+static void	print_status(t_philo_state status, t_philo *philo, long elapsed)
 {
-    if (TAKE_FIRST_FORK == status && !simulation_finished(philo->table))
-        printf("%-6ld %d Has taken fork1\n", elapsed, philo->id);
-    else if(TAKE_SECOND_FORK == status && !simulation_finished(philo->table))
-        printf("%-6ld %d Has taken fork2\n", elapsed, philo->id);
-    else if(EATING == status && !simulation_finished(philo->table))
-        printf("%-6ld %d Is eating\n", elapsed, philo->id);
-    else if(SLEEPING == status && !simulation_finished(philo->table))
-        printf("%-6ld %d Is sleeping\n", elapsed, philo->id);
-    else if(THINKING == status && !simulation_finished(philo->table))
-        printf("%-6ld %d Is thinking\n", elapsed, philo->id);
-    else if(DIED == status && !simulation_finished(philo->table))
-        printf("%-6ld %d died\n", elapsed, philo->id);
+	if (TAKE_FIRST_FORK == status || TAKE_SECOND_FORK == status
+		&& !simulation_finished(philo->table))
+		printf("%-6ld %d has taken a fork\n", elapsed, philo->id);
+	else if (EATING == status && !simulation_finished(philo->table))
+		printf("%-6ld %d is eating\n", elapsed, philo->id);
+	else if (SLEEPING == status && !simulation_finished(philo->table))
+		printf("%-6ld %d is sleeping\n", elapsed, philo->id);
+	else if (THINKING == status && !simulation_finished(philo->table))
+		printf("%-6ld %d is thinking\n", elapsed, philo->id);
+	else if (DIED == status && !simulation_finished(philo->table))
+		printf("%-6ld %d died\n", elapsed, philo->id);
 }
 
-void write_status(t_philo_state status, t_philo *philo, bool debug)
+static void	debug_status(t_philo_state status, t_philo *philo, long elapsed)
 {
-    long elapsed;
+	if (TAKE_FIRST_FORK == status && !simulation_finished(philo->table))
+		printf("%-6ld %d Has taken fork1\n", elapsed, philo->id);
+	else if (TAKE_SECOND_FORK == status && !simulation_finished(philo->table))
+		printf("%-6ld %d Has taken fork2\n", elapsed, philo->id);
+	else if (EATING == status && !simulation_finished(philo->table))
+		printf("%-6ld %d Is eating\n", elapsed, philo->id);
+	else if (SLEEPING == status && !simulation_finished(philo->table))
+		printf("%-6ld %d Is sleeping\n", elapsed, philo->id);
+	else if (THINKING == status && !simulation_finished(philo->table))
+		printf("%-6ld %d Is thinking\n", elapsed, philo->id);
+	else if (DIED == status && !simulation_finished(philo->table))
+		printf("%-6ld %d died\n", elapsed, philo->id);
+}
 
-    safe_mutex_handle(&philo->table->write_mutex, LOCK);
-    elapsed = gettime(MILISECOND) - philo->table->start_simulation_time;
-    if(philo->full)
-    {
-        safe_mutex_handle(&philo->table->write_mutex, UNLOCK);
-        return ;
-    }
-    if(debug)   
-        debug_status(status, philo, elapsed);
-    else
-    {
-        if (TAKE_FIRST_FORK == status || TAKE_SECOND_FORK == status
-            && !simulation_finished(philo->table))
-            printf("%-6ld %d has taken a fork\n", elapsed, philo->id);
-        else if(EATING == status && !simulation_finished(philo->table))
-            printf("%-6ld %d is eating\n", elapsed, philo->id);
-        else if(SLEEPING == status && !simulation_finished(philo->table))
-            printf("%-6ld %d is sleeping\n", elapsed, philo->id);
-        else if(THINKING == status && !simulation_finished(philo->table))
-            printf("%-6ld %d is thinking\n", elapsed, philo->id);
-        else if(DIED == status && !simulation_finished(philo->table))
-            printf("%-6ld %d died\n", elapsed, philo->id);
-    }
-    safe_mutex_handle(&philo->table->write_mutex, UNLOCK);
+void	write_status(t_philo_state status, t_philo *philo, bool debug)
+{
+	long	elapsed;
+
+	safe_mutex_handle(&philo->table->write_mutex, LOCK);
+	elapsed = gettime(MILISECOND) - philo->table->start_simulation_time;
+	if (philo->full)
+	{
+		safe_mutex_handle(&philo->table->write_mutex, UNLOCK);
+		return ;
+	}
+	if (debug)
+		debug_status(status, philo, elapsed);
+	else
+	{
+		print_status(status, philo, elapsed);
+	}
+	safe_mutex_handle(&philo->table->write_mutex, UNLOCK);
 }
 
 void	check_sim_type(t_table *table)
